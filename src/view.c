c21fd98d03d1077b652e7636b3618e65813f0d5a
Seungheon Oh <seungheon.ooh@gmail.com>
2024-02-12 13:58:22 -0600

    a

---
+++ src/view.c
@@ -0,0 +1,35 @@
+#include "view.h"
+
+struct View *mkView(struct DeskServer *container, struct wlr_xdg_surface *data){
+  if(data->role == WLR_XDG_SURFACE_ROLE_POPUP) LOG("is a popup");
+  else LOG("not a popup");
+
+  struct View *view = calloc(1, sizeof(struct View));
+  view->server = container;
+  view->xdgToplevel = data->toplevel;
+
+  ATTACH(View, view, data->surface->events.map, map);
+  ATTACH(View, view, data->surface->events.unmap, unmap);
+  ATTACH(View, view, data->surface->events.destroy, destroy);
+  //  ATTACH(View, view, data->surface->events.map, map);  
+}
+void destryView(struct View *container){
+}
+
+HANDLE(map, void, View) {
+  LOG("MAPMAPMAMPM");
+  wl_list_insert(&container->server->views, &container->link);
+  wlr_xdg_toplevel_set_size(container->xdgToplevel, 720, 720);
+}
+HANDLE(unmap, void, View) {
+}
+HANDLE(destroy, void, View) {
+}
+HANDLE(requestMove, void, View) {
+}
+HANDLE(requestResize, void, View) {
+}
+HANDLE(requestMaximize, void, View) {
+}
+HANDLE(requestFullscreen, void, View) {
+}
---
+++ src/view.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "imports.h"
+#include "server.h"
+#include "events.h"
+#include <time.h>
+#include <math.h>
+
+/*
+  Basically a window
+ */
+typedef struct View {
+  struct DeskServer *server;
+  struct wl_list link;
+  struct wlr_xdg_toplevel *xdgToplevel;  
+  struct wl_listener map;
+  struct wl_listener unmap;
+  struct wl_listener destroy;
+  struct wl_listener requestMove;
+  struct wl_listener requestResize;
+  struct wl_listener requestMaximize;
+  struct wl_listener requestFullscreen;
+
+  int x, y;
+} View;
+
+struct View *mkView(struct DeskServer*, struct wlr_xdg_surface*);
+void destryView(struct View *);
+
+LISTNER(map, void, View)
+LISTNER(unmap, void, View)
+LISTNER(destroy, void, View)
+LISTNER(requestMove, void, View)
+LISTNER(requestResize, void, View)
+LISTNER(requestMaximize, void, View)
+LISTNER(requestFullscreen, void, View)
